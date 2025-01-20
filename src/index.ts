7fec3dc3a1250e8cff6d6c61603499f7a42acdb5
Alex Avila <alex.avila.09@outlook.com>
2025-01-20 00:30:35 -0700

    feat: write task routes w/db crud operations, error handling, & basic validation

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,10 @@
 import type { Application } from "express";
-import { PrismaClient } from "@prisma/client";
 import cors from "cors";
 import express from "express";
 import { config } from "dotenv";
+import tasksRoutes from "./routes/tasks";
 
 config({ path: ".env" });
-
-const prisma = new PrismaClient();
-
-async function main() {
-  const allTasks = await prisma.task.findMany();
-  console.log(allTasks);
-}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (error) => {
-    console.error(error);
-    await prisma.$disconnect();
-    process.exit();
-  });
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
@@ -29,8 +12,10 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+app.use("/tasks", tasksRoutes);
+
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("Hello world");
 });
 
 app.listen(port, () => {
---
+++ src/routes/tasks.ts
@@ -0,0 +1,96 @@
+import { Router } from "express";
+import { PrismaClient, Prisma } from "@prisma/client";
+import { z } from "zod";
+
+export const TaskCreateInput = z.object({
+  title: z.string().max(255),
+  color: z.string().max(7).optional(),
+}) satisfies z.Schema<Prisma.TaskUncheckedCreateInput>;
+
+export const TaskUpdateInput = z.object({
+  id: z.coerce.number(),
+  title: z.string().max(255).optional(),
+  color: z.string().max(7).optional(),
+  completed: z.boolean().optional(),
+}) satisfies z.Schema<Prisma.TaskUncheckedUpdateInput>;
+
+const router = Router();
+
+const prisma = new PrismaClient().$extends({
+  query: {
+    task: {
+      create({ args, query }) {
+        args.data = TaskCreateInput.parse(args.data);
+        return query(args);
+      },
+      update({ args, query }) {
+        args.data = TaskUpdateInput.partial().parse(args.data);
+        return query(args);
+      },
+    },
+  },
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const allTasks = await prisma.task.findMany();
+
+    res.json(allTasks);
+  } catch (err) {
+    res.json({ error: "Unexpected error: Unable to fetch tasks" });
+  }
+});
+
+router.post(`/`, async (req, res) => {
+  const { title, color } = req.body;
+
+  try {
+    const result = await prisma.task.create({
+      data: {
+        title,
+        ...(color && { color }),
+      },
+    });
+
+    res.json(result);
+  } catch (error) {
+    res.json({ error: "Unexpected error: Unable to create task" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, color, completed } = req.body;
+
+  try {
+    const updatedTask = await prisma.task.update({
+      where: { id: Number(id) || undefined },
+      data: {
+        ...(title && { title }),
+        ...(color && { color }),
+        ...(completed ?? { completed }),
+      },
+    });
+
+    res.json(updatedTask);
+  } catch (error) {
+    res.json({ error: "Unable to update task or it does not exist" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const task = await prisma.task.delete({
+      where: { id: Number(id) || undefined },
+    });
+    res.json(task);
+  } catch (error) {
+    res.json({
+      error: "Unexpected error: Unable to delete task or it doesn't exist",
+    });
+  }
+});
+
+export default router;
