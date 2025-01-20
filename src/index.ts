ea8bd7bc17df173ff3ed77c8d96a612a6651bbbe
Alex Avila <alex.avila.09@outlook.com>
2025-01-19 22:30:44 -0700

    feat(db): set up prisma and test it

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
 import type { Application } from "express";
+import { PrismaClient } from "@prisma/client";
 import cors from "cors";
 import express from "express";
 import { config } from "dotenv";
 
 config({ path: ".env" });
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const allTasks = await prisma.task.findMany();
+  console.log(allTasks);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (error) => {
+    console.error(error);
+    await prisma.$disconnect();
+    process.exit();
+  });
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
